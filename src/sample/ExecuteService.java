d336cfc2b84c8c5b39fafa90f58a997df3a95164
obruno <ese.obruno@gmail.com>
2019-07-22 00:58:52 +0100

    Update to the pase package

--- src/sample/ExecuteService.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-/**
- * Created by Ese on 12/11/2015.
- */
-public interface ExecuteService {
-    public void execute();
-}
--- src/sample/PDrugSalesBeans.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-/**
- * Created by Ese on 1/16/2016.
- */
-public class PDrugSalesBeans {
-
-}
